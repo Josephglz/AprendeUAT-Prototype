21bf4dfd1bae8e458e358dfda8d3e0a79d8e7727
JosephGlz <joseph220199@gmail.com>
2023-10-12 21:02:54 -0600

    feat: Carousel module implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
+
+import { CarouselModule } from '@coreui/angular';
 
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
@@ -15,6 +19,10 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+
+    CarouselModule,
 
     SharedModule,
   ],
