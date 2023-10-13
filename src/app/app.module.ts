0d178a6627f7108b69b14ef05c46e3cc921b6166
JosephGlz <joseph220199@gmail.com>
2023-10-12 20:04:22 -0600

    feat: Routing and components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
