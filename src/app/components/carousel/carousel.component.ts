2a25b236c84cdad9432125ee153ee51861cd7879
JosephGlz <joseph220199@gmail.com>
2023-10-13 00:47:45 -0600

    perfs: Carousel dynamic sized

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-carousel-component',
   templateUrl: './carousel.component.html'
 })
 export class CarouselComponent {
+  @Input({ required: true })
+  height: number = 0;
+
   slides: any[] = new Array(3).fill({id: -1, src: '', title: '', subtitle: ''});
 
   constructor() {}
