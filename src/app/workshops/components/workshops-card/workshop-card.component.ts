8cbbf427daea5833150fbf2880dec5cb901a174b
JosephGlz <joseph220199@gmail.com>
2023-10-13 14:00:28 -0600

    feat: Data retrieveing management

--- src/app/workshops/components/workshops-card/workshop-card.component.ts
+++ src/app/workshops/components/workshops-card/workshop-card.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-workshops-card',
   templateUrl: './workshop-card.component.html'
 })
 export class WorkshopCardComponent {
+  @Input() workshopInfo: any;
 }
--- src/app/workshops/pages/workshops-page/workshops-page.component.ts
+++ src/app/workshops/pages/workshops-page/workshops-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Workshops } from 'src/app/shared/interfaces/Workshops';
 
 @Component({
   selector: 'app-workshops-page',
@@ -20,4 +21,19 @@ export class WorkshopsPageComponent {
   toggleFilterSubjects() {
     this.filterSubjects = !this.filterSubjects;
   }
+
+  workshopsList: Workshops[] = [
+    {
+      title: 'ChatBOT en la enseñanza',
+      description: 'Aprende a utilizar un chatbot para tu clase',
+      resourcesLinks: [
+        {
+          title: '001 ChatBOT Presentación',
+          path: 'https://alumnosuatedu-my.sharepoint.com/:v:/r/personal/coordsistfi_uat_edu_mx/Documents/talleres/CHatbootFI/001.-ChatBotPresentacion.mp4?csf=1&web=1&nav=eyJyZWZlcnJhbEluZm8iOnsicmVmZXJyYWxBcHAiOiJPbmVEcml2ZUZvckJ1c2luZXNzIiwicmVmZXJyYWxBcHBQbGF0Zm9ybSI6IldlYiIsInJlZmVycmFsTW9kZSI6InZpZXciLCJyZWZlcnJhbFZpZXciOiJNeUZpbGVzTGlua0RpcmVjdCJ9fQ&e=mMa2hP',
+          author: 'Coordinación de la carrera de Ingeniería en sistemas computacionales',
+          type: 'Video'
+        }
+      ]
+    }
+  ]
 }
