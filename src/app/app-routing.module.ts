e52813418d3d0a86132824d92608155a184ef8db
JosephGlz <joseph220199@gmail.com>
2023-10-13 01:47:05 -0600

    feat: Tutorials route added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { HomePageComponent } from './pages/home-page/home-page.component';
-
-
+import { TutorialsPageComponent } from './tutorials/pages/tutorials-page/tutorials-page.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
+  { path: 'tutorias', component: TutorialsPageComponent}
 ];
 
 @NgModule({
