cffef64c55b5ae3eeed27fb91fe4dfd45e5e3e4a
JosephGlz <joseph220199@gmail.com>
2023-10-12 21:03:09 -0600

    feat: carousel component added

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -4,5 +4,20 @@ import { Component } from '@angular/core';
   templateUrl: './home-page.component.html'
 })
 export class HomePageComponent {
+  slides: any[] = new Array(3).fill({id: -1, src: '', title: '', subtitle: ''});
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.slides[0] = {
+      src: '/assets/images/banners/banneruno.png',
+    };
+    this.slides[1] = {
+      src: '/assets/images/banners/bannerdos.jpg',
+    }
+    this.slides[2] = {
+      src: '/assets/images/banners/bannertres.png',
+    }
+  }
 
 }
