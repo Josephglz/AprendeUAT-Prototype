e622bbae538b76ee7d4c8e5243e1ed995f39f3e4
JosephGlz <joseph220199@gmail.com>
2023-10-13 11:36:21 -0600

    feat: Card component template

---
+++ src/app/tutorials/components/tutorials-card/tutorials-card.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tutorials-card',
+  templateUrl: './tutorials-card.component.html'
+})
+export class TutorialsCardComponent {
+}
