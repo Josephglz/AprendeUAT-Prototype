59fe27104f4b8c02532e352112e97bf642fc9a47
JosephGlz <joseph220199@gmail.com>
2023-10-14 01:29:53 -0600

    feat: Details page for tutorials added

--- src/app/tutorial/pages/tutorials-details/tutorials-details.component.ts
+++ src/app/tutorial/pages/tutorials-details/tutorials-details.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { TutorialsService } from '../../services/tutorials.service';
+import { Tutorials } from 'src/app/shared/interfaces/Tutorials';
 
 @Component({
   templateUrl: './tutorials-details.component.html'
 })
 export class TutorialsDetailsComponent {
+  idx: number = 0;
+  tutorialInfo: Tutorials = {} as Tutorials;
 
+  constructor(
+    private _routeData: ActivatedRoute,
+    private _tutorialService: TutorialsService
+  ) { }
+
+  ngOnInit(): void {
+    this._routeData.params.subscribe(params => {
+      this.idx = +params['id'];
+      this.tutorialInfo = this._tutorialService.getTutorialsById(this.idx)!;
+    });
+  }
 }
